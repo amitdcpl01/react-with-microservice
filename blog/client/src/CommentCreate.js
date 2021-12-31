79315e02e551971680e55aa47bc3e888d6a259f2
Amit Kumar <amit.kumar13@bp.com>
2022-01-01 02:56:28 +0530

    comment added

--- blog/client/src/CommentCreate.js
+++ blog/client/src/CommentCreate.js
@@ -2,16 +2,15 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 export default ({ postId }) => {
-  const [content, setContent] = useState('');
+  const [contentA, setContentA] = useState('');
 
   const onSubmit = async event => {
     event.preventDefault();
-
     await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
-      content
+      content: contentA
     });
 
-    setContent('');
+    setContentA('');
   };
 
   return (
@@ -20,8 +19,8 @@ export default ({ postId }) => {
         <div className="form-group">
           <label>New Comment</label>
           <input
-            value={content}
-            onChange={e => setContent(e.target.value)}
+            value={contentA}
+            onChange={e => setContentA(e.target.value)}
             className="form-control"
           />
         </div>
